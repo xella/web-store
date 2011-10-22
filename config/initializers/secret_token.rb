95e77748ff8ec788e05f87f3fc6ca66f27beb02b
Eicca <eicca@kubuntu.(none)>
2011-10-22 17:23:16 +0400

    initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebStore::Application.config.secret_token = '6fa45ee4c2517447bb48bfc6942f35c9dead46f2fa542be73fa60561eefabfbf4b5701acbca24b96f7892fca071d57c3270bab04c389a50de3c74dfb993416a8'
